24398597a7e651c3839fff724b5acfed6503c23f
whiteh34rt <54319779+whiteh34rt@users.noreply.github.com>
2020-10-17 20:24:31 +0100

    Update report.php

--- report.php
+++ report.php
@@ -7,33 +7,47 @@
         <form id='file' action="report.php" method='post' accept-charset='UTF-8'>
             <div >
                         <label for='file' >File : (name.extension)</label>
-  				<input type='text' name='file' id='report' maxlength="50" required />
+                <input type='text' name='file' id='report' maxlength="50" required />
 
                     <input type='submit' class="btn btn-default" name='Read' id='submit' value='Read' />
            </div>
+           </form>
 
-			<?php
+           
+        
+        <h1> List of Files</h1>
+        <table class="table table-bordered">
+            <thead>
+            <tr style="background-color:#337ab7;">
+                <td><font color="white"> File Name</font> </td>
+            </tr>
+            </thead>
+            <tbody>
+                 <?php
 $dir    = 'reports/';
 $files1 = scandir($dir);
 $files2 = scandir($dir, 1);
 foreach($files1 as $key) {
-    echo $key;
+    if($key!="." and $key!=".."){
+            echo " <tr style='background-color:#fafafa;opacity:0.7;'>";
+        echo " <td>". $key ." </td>";
+        echo "</tr>";}
 }
 //print_r($files1);
 //print_r($files2);
 ?>
+</tbody></table>
 
-        </form>
-        
+
 <?php
 if(!empty($_POST['file'])){
-	if( preg_match("/^[a-zA-Z0-9.]+$/",$_POST["file"])) {
-	    $homepage = file_get_contents("reports/".$_POST["file"]);
-	    echo $homepage;
-		
-	}else{
-	    echo "The file name you entered is not valid";
-	}
+    if( preg_match("/^[a-zA-Z0-9.]+$/",$_POST["file"])) {
+        $homepage = file_get_contents("reports/".$_POST["file"]);
+        echo $homepage;
+        
+    }else{
+        echo "The file name you entered is not valid";
+    }
 }
 ?>
         </div>
